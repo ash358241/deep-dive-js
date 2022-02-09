2801083aad4fd0a6ed37ef8ff71b7e30d3c2c8c7
Ashraful Islam <ashrafunit7@gmail.com>
2022-02-09 18:38:08 +0600

    theme is contained to local storage

--- Projects/LIGHT-N-DARK-MODE/script.js
+++ Projects/LIGHT-N-DARK-MODE/script.js
@@ -40,12 +40,24 @@ function switchTheme(e) {
     // console.log(e.target.checked);
     if(e.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark'); 
+        localStorage.setItem('theme', 'dark');
         darkMode();  
     }else{
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
 
 //event listeners
 toggleSwitch.addEventListener('change', switchTheme);
+
+//check local storage for theme 
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme){
+    document.documentElement.setAttribute('data-theme', currentTheme); 
+    if(currentTheme === 'dark'){
+        toggleSwitch.checked = true;
+        darkMode();  
+    }
+}
